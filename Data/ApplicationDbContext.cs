143f704d4b32dd67af81298032f4b688a73d06db
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-02-17 08:03:17 -0600

    Completed the (Hero) Model and Set up the Applicationdbcontext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperHeroProject.Models;
 
 namespace SuperHeroProject.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Hero> Heroes { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ Models/Hero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroProject.Models
+{
+    public class Hero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Heroname { get; set; }
+        public string Herorealname { get; set; }
+        public string Heroprimarypower { get; set; }
+        public string Herosecondarypower { get; set; }
+        public string catchphrase { get; set; }
+    }
+}
