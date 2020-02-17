b3b21f837b502ecd49e5c9feb57383800ca58b82
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-02-17 09:52:38 -0600

    Created my DbSet

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperHero_WebApplication.Models;
 
 namespace SuperHero_WebApplication.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Hero> Heroes;
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
